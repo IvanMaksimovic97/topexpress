18d21698c34ff6804dbd6ae14a0f2a2a4b4ccd49
Ivan Maksimović <imaksimovic97@gmail.com>
2022-05-03 21:09:01 +0200

    ubacen sajt

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -10,4 +10,9 @@ class SiteController extends Controller
     {
         return view('site.index');
     }
+
+    public function contact()
+    {
+        return view('site.contact');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/postarina/vrsta/{id_vrsta}/masa/{masa}', 'CenovnikController@dohvatiCenuPostarine')->name('cena-postarine');
 Route::get('/broj-posiljke-validacija/{broj?}', 'PosiljkaController@proveraBrojaPosiljke')->name('broj-posiljke-validacija');
 Route::get('/', 'SiteController@index')->name('index');
+Route::get('/kontakt', 'SiteController@contact')->name('contact');
 
 Route::group([
     'middleware' => ['cmsAuth'],
