16577c5d5121a341c4d45de36ce7dd2f39cd1c68
Ivan Maksimović <imaksimovic97@gmail.com>
2022-11-21 23:56:35 +0100

    seeder za korisnike

--- database/seeds/KorisnikSeeder.php
+++ database/seeds/KorisnikSeeder.php
@@ -14,28 +14,55 @@ class KorisnikSeeder extends Seeder
      */
     public function run()
     {
+        // $korisnik = new Korisnik;
+        // $korisnik->email = 'office@topexpress.rs';
+        // $korisnik->password = Hash::make('topexpress2022');
+        // $korisnik->ime = 'Duško';
+        // $korisnik->prezime = 'Bosnić';
+        // $korisnik->created_at = Carbon::now();
+        // $korisnik->save();
+
+        // $korisnik = new Korisnik;
+        // $korisnik->email = 'sekularacs@gmail.com';
+        // $korisnik->password = Hash::make('topexpress2022');
+        // $korisnik->ime = 'Simo';
+        // $korisnik->prezime = 'Šekularac';
+        // $korisnik->created_at = Carbon::now();
+        // $korisnik->save();
+
+        // $korisnik = new Korisnik;
+        // $korisnik->email = 'imaksimovic97@gmail.com';
+        // $korisnik->password = Hash::make('topexpress2022');
+        // $korisnik->ime = 'Ivan';
+        // $korisnik->prezime = 'Maksimović';
+        // $korisnik->created_at = Carbon::now();
+        // $korisnik->save();
+
         $korisnik = new Korisnik;
-        $korisnik->email = 'office@topexpress.rs';
-        $korisnik->password = Hash::make('topexpress2022');
-        $korisnik->ime = 'Duško';
-        $korisnik->prezime = 'Bosnić';
+        $korisnik->email = 'sasaradevic@topexpress.com';
+        $korisnik->password = Hash::make('sasaradevic2022');
+        $korisnik->ime = 'Saša';
+        $korisnik->prezime = 'Radević';
         $korisnik->created_at = Carbon::now();
+        $korisnik->pristup = 1;
         $korisnik->save();
 
         $korisnik = new Korisnik;
-        $korisnik->email = 'sekularacs@gmail.com';
-        $korisnik->password = Hash::make('topexpress2022');
-        $korisnik->ime = 'Simo';
-        $korisnik->prezime = 'Šekularac';
+        $korisnik->email = 'sarajovanovic@topexpress.com';
+        $korisnik->password = Hash::make('sarajovanovic2022');
+        $korisnik->ime = 'Sara';
+        $korisnik->prezime = 'Jovanović';
         $korisnik->created_at = Carbon::now();
+        $korisnik->pristup = 1;
         $korisnik->save();
 
         $korisnik = new Korisnik;
-        $korisnik->email = 'imaksimovic97@gmail.com';
-        $korisnik->password = Hash::make('topexpress2022');
-        $korisnik->ime = 'Ivan';
-        $korisnik->prezime = 'Maksimović';
+        $korisnik->email = 'dejandrobnjak@topexpress.com';
+        $korisnik->password = Hash::make('dejandrobnjak2022');
+        $korisnik->ime = 'Dejan';
+        $korisnik->prezime = 'Drobnjak';
         $korisnik->created_at = Carbon::now();
+        $korisnik->pristup = 1;
         $korisnik->save();
     }
 }
